49c7258e0c0a681c22aac6d7a127e9439c51e420
Azure0413 <88129659+Azure0413@users.noreply.github.com>
2022-09-16 15:33:41 +0800

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div className='footer'>陳冠言 2022 ©</div>
+  )
+}
+export default Footer;
---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div style={{minHeight : "100vh"}}>
+        <h1>About</h1>
+        <p>My name is 陳冠言</p>
+        <p>To look my profile. Please go to another project </p>
+    </div>
+  )
+}
+export default About;
